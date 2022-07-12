b29162681a6e2a2ea6f0d5e2d34dfbfde5479aad
Moisés <moisesnetored@gmail.com>
2022-07-12 17:49:02 -0400

    feat: Landing page created.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,39 @@
+import {FiArrowRight} from 'react-icons/fi';
+
+
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+
+import logoImg from './assets/images/logo.svg';
+
 export function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+
+          <p>
+            Visite orfanatos e mude o dia de muitas crianças.
+          </p>
+        </main>
+
+        <div className="location">
+          <strong>
+            Manaus
+          </strong>
+          <span>
+            Amazonas
+          </span>
+        </div>
+
+        <a href="/#" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
