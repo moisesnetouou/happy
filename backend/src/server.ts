d8e73b96e94b07ca864a8c1d292c40ead3c4c249
Moisés <moisesnetored@gmail.com>
2022-07-13 17:30:01 -0400

    feat: Cors tweaks, database deleted, new database re-run.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ import routes from './routes';
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
