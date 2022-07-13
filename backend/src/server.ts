42eefdee4733ff0ad2ea2d5b4ecf71bafa0ef30e
Moisés <moisesnetored@gmail.com>
2022-07-13 14:39:53 -0400

    feat: Working with views and handling exceptions.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import 'express-async-errors';
+import path from 'path';
 
 import './database/connection';
+import errorHandler from './errors/handler';
 import routes from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 
 app.listen(3333);
