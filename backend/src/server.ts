0c64f5db27ceb8240114176610284816935bf487
Moisés Neto <moisesnetored@gmail.com>
2022-07-16 17:58:22 -0400

    add: PORT default

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,6 +15,6 @@ app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler);
 
-app.listen(3333, () => {
-  console.log("Server: ok")
-});
+app.listen(process.env.PORT || 3333, () => {
+  console.log("HTTP server running!")
+})
